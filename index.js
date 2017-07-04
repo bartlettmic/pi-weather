cd981594e6480a6413162a61c20ec35754e1ae36
Michael Bartlett <bartlettmic@gmail.com>
2017-07-04 22:44:27 +0000

    Glorious timeout!

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const particle = new Particle();
 const token = 'ab3e0bb7c384847246e3c8276afa54adbf5971f4';
 const ID = '3b0035000247353137323334';
 
-var schedule = require('node-schedule');
-schedule.scheduleJob('*/5 * * * *', function() {
+//var schedule = require('node-schedule');
+//schedule.scheduleJob('*/5 * * * *', function() {
     var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 4 });
     camera.start();
-    camera.on("read", function(err, filename) { setTimeout(camera.stop(), camera.get("timeout") * 2) });
-
-    setTimeout(function() { camera.stop() }, camera.get("timeout") * 2);
+    camera.on("read", function(err, filename) { setTimeout( function() { camera.stop(); console.log("exitting..."); }, camera.get("timeout") * 2000) });
 
     particle.getDevice({ deviceId: ID, auth: token }).then(function(data) {
             var promises = [];
@@ -30,4 +28,4 @@ schedule.scheduleJob('*/5 * * * *', function() {
             console.log('API call failed.');
         }
     );
-});
+//});
