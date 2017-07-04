8a62982dc17c1c571fd231158d590209e03c6005
Michael Bartlett <bartlettmic@gmail.com>
2017-07-04 15:50:05 -0600

    die camera die

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const ID = '3b0035000247353137323334';
 // var schedule = require('node-schedule');
 // var j = schedule.scheduleJob('*/5 * * * *', function() {
 var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 4 });
-setTimeout(function() { camera.start() }, 4000);
-// camera.on("exited", function() { camera.stop(); });
+camera.start();
+setTimeout(camera.stop(), camera.get("timeout"));
 
 particle.getDevice({ deviceId: ID, auth: token }).then(function(data) {
         var promises = [];
