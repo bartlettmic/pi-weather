2f1220b436a02e70bb69b428bd9a1e83ca6ffefc
Michael Bartlett <bartlettmic@gmail.com>
2017-07-04 21:30:51 +0000

    test

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const ID = '3b0035000247353137323334';
 
 // var schedule = require('node-schedule');
 // var j = schedule.scheduleJob('*/5 * * * *', function() {
-var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", t: 1 });
+var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 4 });
 camera.start();
-camera.stop();
+camera.on("exited", function() { camera.stop(); });
 
 particle.getDevice({ deviceId: ID, auth: token }).then(function(data) {
         var promises = [];
