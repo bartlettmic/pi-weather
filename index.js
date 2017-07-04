ba380ce5fc820b53619d841da830d716be1cad56
Michael Bartlett <bartlettmic@gmail.com>
2017-07-04 21:39:33 +0000

    cmon

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const ID = '3b0035000247353137323334';
 var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 4 });
 camera.start();
 camera.on("exited", function() { camera.stop(); });
-
+//
 particle.getDevice({ deviceId: ID, auth: token }).then(function(data) {
         var promises = [];
         for (var v of Object.getOwnPropertyNames(data.body.variables)) promises.push(particle.getVariable({ deviceId: ID, name: v, auth: token }));
