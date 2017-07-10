b50e8ff8a090bce1102175ebd98a6c8cb570ef1f
Michael Bartlett <bartlettmic@gmail.com>
2017-07-10 15:02:58 -0600

    TELL ME THE PROMISES

--- weather.js
+++ weather.js
@@ -6,10 +6,10 @@ const config = require('./config.json');
 
 //var schedule = require('node-schedule');
 //schedule.scheduleJob('*/5 * * * *', function() {
-var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 1000 });
+var camera = new RaspiCam({ mode: "photo", output: "/var/www/html/snapshot.png", w: 960, h: 540, e: "png", timeout: 2000 });
 camera.start();
 camera.on("read", function(err, filename) {
-  setTimeout(function() {
+    setTimeout(function() {
         camera.stop();
         console.log("exitting...");
     }, camera.get("timeout"))
@@ -19,17 +19,22 @@ console.log(config);
 
 particle.getDevice({ deviceId: config.ID, auth: config.token }).then(function(data) {
         var promises = [];
-        for (var v of Object.getOwnPropertyNames(data.body.variables)) promises.push(particle.getVariable({ deviceId: config.ID, name: v, auth: config.token }).then(console.log(data)));
+        for (var v of Object.getOwnPropertyNames(data.body.variables)) {
+            promises.push(
+                particle.getVariable({ deviceId: config.ID, name: v, auth: config.token })
+                .then(console.log(data.body.name))
+            );
+        }
         Promise.all(promises).then(values => {
             var output = {};
             for (var v of values) output[v.body.name] = v.body.result;
             output.timestamp = new Date().toLocaleString();
             fs.writeFileSync('weather.json', JSON.stringify(output), 'utf8');
-	    console.log("Current weather recorded.");
+            console.log("Current weather recorded.");
         })
         .catch(function(err) {
             console.log("Unable to resolve all promises."); // some coding error in handling happened
-	    console.log(err);
+            console.log(err);
         });
     },
     function(err) {
