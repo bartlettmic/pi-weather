95ea8e27e9a0b6ee3d472af308e159df76e4363b
Michael Bartlett <bartlettmic@gmail.com>
2017-07-10 15:34:53 -0600

    plz

--- weather.js
+++ weather.js
@@ -22,8 +22,10 @@ particle.getDevice({ deviceId: config.ID, auth: config.token }).then(function(da
         for (var v of Object.getOwnPropertyNames(data.body.variables)) {
             promises.push(
                 particle.getVariable({ deviceId: config.ID, name: v, auth: config.token })
-                .then(function(data) { console.log(data.body.name) }, function(err) { console.log(err) })
-                );
+                .then(function(data) {
+                    try { console.log(data.body.name) } catch(e) {}
+                }, function(err) { console.log(err) })
+            );
         }
         Promise.all(promises).then(values => {
             var output = {};
