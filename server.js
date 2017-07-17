4525a1a7470a0d0f7cee5160a0b244167eae6db5
Michael Bartlett <bartlettmic@gmail.com>
2017-07-16 19:20:13 -0600

    Let's go express

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-// require all dependencies
-var express = require('express');
-var app = express();
- 
-// set up the template engine
+var app = require('express')();
 app.set('views', './');
 app.set('view engine', 'pug');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
  
-// GET response for '/'
-app.get('/', function (req, res) {
- 
-    // render the 'index' template, and pass in a few variables
-    res.render('index', { title: 'Hey', message: 'Hello there!' });
- 
-});
- 
-// start up the server
-app.listen(8080, function () {
-    console.log('Listening on http://localhost:8080');
-});
+app.listen(8080, function () { console.log('Listening on http://localhost:8080'); });
+
+app.get('/', function (req, res) { res.render('index', { title: 'Hey', message: 'Hello there!' }); });
+
+app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
+
+io.on('connection', function(socket){ socket.on('chat message', function(msg){ io.emit('chat message', msg); }); });
+
+http.listen(8080,function(){ console.log('listening on *:8080'); });
