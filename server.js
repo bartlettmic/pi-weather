5d5ecdfe80b3a4d30ef76d2237b0900bbd4e30f5
Michael Bartlett <bartlettmic@gmail.com>
2017-07-16 20:56:39 -0600

    Work with me express

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ app.set('view engine', 'pug');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
  
-app.listen(8080, function () { console.log('Listening on http://localhost:8080'); });
+app.listen(8000, function () { console.log('Listening on http://localhost:8080'); });
 
 app.get('/', function (req, res) { res.render('index', { title: 'Hey', message: 'Hello there!' }); });
 
-app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
+// app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html'); });
 
-io.on('connection', function(socket){ socket.on('chat message', function(msg){ io.emit('chat message', msg); }); });
+// io.on('connection', function(socket){ socket.on('chat message', function(msg){ io.emit('chat message', msg); }); });
 
-http.listen(8080,function(){ console.log('listening on *:8080'); });
+// http.listen(8080,function(){ console.log('listening on *:8000'); });
