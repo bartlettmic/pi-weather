09204a7a2214d4cbc27df54ad70fe8ab586ef0c9
Michael Bartlett <bartlettmic@gmail.com>
2017-07-17 10:09:11 -0600

    Better timestamp?

--- weather.js
+++ weather.js
@@ -12,35 +12,33 @@ try {
     var camera = new RaspiCam({ mode: "photo", output: "/home/pi/pi-weather/snapshot.png", w: 960, h: 540, e: "png", timeout: 2000 });
     camera.start();
     camera.on("read", function(err, filename) {
-        setTimeout(function() {
-            camera.stop();
-            console.log("exitting...");
-        }, camera.get("timeout"))
+        setTimeout(function() { console.log("exitting..."), camera.stop(); }, camera.get("timeout"))
     });
-} catch (err) { console.log("Unable to access Pi Camera module.") };
-
-particle.callFunction({ deviceId: config.ID, name: 'update', argument: '', auth: config.token })
-    .then(function(data) {
-        console.log('Station updated, pulling sensor data...');
-        particle.getDevice({ deviceId: config.ID, auth: config.token }).then(
-            function(data) {
-                var promises = [];
-                for (var v of Object.getOwnPropertyNames(data.body.variables)) {
-                    promises.push(particle.getVariable({ deviceId: config.ID, name: v, auth: config.token })
-                        .then(function(data) {
-                            process.stdout.write(data.body.name + " ")
-                            return data;
-                        }, function(err) { console.log(err) })
-                    )
-                }
-                Promise.all(promises).then(values => {
-                    var output = {};
-                    for (var v of values) output[v.body.name] = v.body.result;
-                    output.timestamp = new Date().toLocaleString();
-                    fs.writeFileSync('weather.json', JSON.stringify(output, null, "\t"), 'utf8');
-                    console.log("\nCurrent sensor values recorded.");
-                }).catch(function(err) { console.log("Unable to resolve all promises.", err); })
-            },
-            function(err) { console.log('Device call failed.', err); });
-    }, function(err) { console.log('Unable to update station.', err); });
+} catch (err) { console.log("Unable to access Pi Camera module.") } finally {
+    particle.callFunction({ deviceId: config.ID, name: 'update', argument: '', auth: config.token })
+        .then(function(data) {
+            console.log('Station updated, pulling sensor data...');
+            particle.getDevice({ deviceId: config.ID, auth: config.token }).then(function(data) {
+                    var promises = [];
+                    for (var v of Object.getOwnPropertyNames(data.body.variables)) {
+                        promises.push(particle.getVariable({ deviceId: config.ID, name: v, auth: config.token })
+                            .then(function(data) {
+                                var _trunc = (+data.body.result).toFixed(2);
+                                if (!isNaN(_trunc)) data.body.result = _trunc;
+                                process.stdout.write(data.body.name + "=" + data.body.result + " ")
+                                return data;
+                            }, function(err) { console.log(err) })
+                        )
+                    }
+                    Promise.all(promises).then(values => {
+                        var output = {};
+                        for (var v of values) output[v.body.name] = v.body.result;
+                        output.timestamp = new Date().toLocaleString({timezone: null});
+                        fs.writeFileSync('weather.json', JSON.stringify(output, null, "\t"), 'utf8');
+                        console.log("\nCurrent sensor values recorded.");
+                    }).catch(function(err) { console.log("Unable to resolve all promises.", err); })
+                },
+                function(err) { console.log('Device call failed.', err); });
+        }, function(err) { console.log('Unable to update station.', err); });
+}
 //});
