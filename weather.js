a8fbfe369b2bfc2119c6d5780628381966f666d9
Michael Bartlett <bartlettmic@gmail.com>
2017-09-11 11:35:30 -0600

    Cleanup weather.js

--- weather.js
+++ weather.js
@@ -3,26 +3,17 @@ const Particle = require('particle-api-js');
 const particle = new Particle();
 const config = require('./config.json');
 const Raspistill = require('node-raspistill').Raspistill;
+const camera = new Raspistill({
+    outputDir: './',
+    fileName: "snapshot",
+    width: 960,
+    height: 540,
+    encoding: "png",
+});
+camera.takePhoto().then(() => { console.log("Photo captured") }).catch((err) => { console.log("Unable to access Pi Camera module.") })
 
-// try {
-    const camera = new Raspistill({
-        outputDir: './',
-        fileName: "snapshot.png",
-        width: 960,
-        height: 540,
-        encoding: "png",
-    });
-    camera.takePhoto().then(() => { console.log("Photo captured")}).catch((err) => { console.log("Unable to access Pi Camera module.") })
-    // camera.start();
-    // camera.on("start", function(err, filename) {
-    //     setTimeout(function() { console.log("exitting..."), camera.stop(); }, camera.get("timeout") * 3.0)
-    // });
-    // camera.on("read", function(err, filename) {
-    //     setTimeout(function() { console.log("exitting..."), camera.stop(); }, camera.get("timeout"))
-    // });
-// } catch (err) { console.log("Unable to access Pi Camera module.") } finally {
-    particle.callFunction({ deviceId: config.ID, name: 'update', argument: '', auth: config.token })
-        .then(function(data) {
+particle.callFunction({ deviceId: config.ID, name: 'update', argument: '', auth: config.token })
+    .then(function(data) {
             console.log('Station updated, pulling sensor data...');
             particle.getDevice({ deviceId: config.ID, auth: config.token }).then(function(data) {
                     var promises = [];
@@ -41,9 +32,9 @@ const Raspistill = require('node-raspistill').Raspistill;
                         for (var v of values) output[v.body.name] = v.body.result;
                         output.timestamp = new Date().toLocaleString();
                         fs.writeFileSync('weather.json', JSON.stringify(output, null, "\t"), 'utf8');
-                        console.log("\nCurrent sensor values recorded.");
+                        console.log("\nCurrent sensor values recorded. (" + output.timestamp +")");
                     }).catch(function(err) { console.log("Unable to resolve all promises.", err); })
                 },
                 function(err) { console.log('Device call failed.', err); });
-        }, function(err) { console.log('Unable to update station.', err); });
-// }
+        },
+        function(err) { console.log('Unable to update station.', err); });
