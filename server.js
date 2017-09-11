b4e37114fb9e5d27515c60d4e8069c0e94827c77
Michael Bartlett <bartlettmic@gmail.com>
2017-09-11 09:26:41 -0600

    upgrades

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('view engine', 'pug');
 app.use(exp.static('./'))
 var http = require('http').Server(app);
 var childProcess = require('child_process');
-var process = childProcess.fork("./weather.js");
+// var process = childProcess.fork("./weather.js");
 
 // var io = require('socket.io')(http);
 
@@ -13,9 +13,9 @@ var fs = require('fs');
 var _buf = fs.readFileSync('snapshot.png', {}, function(err, buf) { return buf });
 var _md5 = require('md5')(_buf);
 
-var _port = 3000;
+var _port =8080;
  
-app.listen(_port, function () { console.log('Listening on '+_port); });
+app.listen(_port, () => { console.log('Listening on '+_port); });
 
 app.get('/', function (req, res) { res.render('index', {_md5:_md5}); });
 
